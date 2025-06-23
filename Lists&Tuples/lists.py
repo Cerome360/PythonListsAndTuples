7ca44c8db859c5338f4434794e9cd0c265762437
Cerome360 <61514307+Cerome360@users.noreply.github.com>
2025-06-23 10:54:04 +0100

    Add files via upload

---
+++ Lists&Tuples/lists.py
@@ -0,0 +1,43 @@
+# users = ['Dave', 'John', 'Sara']
+# print(users)
+# print(type(users))
+
+# users.append('Rebirth')
+# print(users)
+
+# users.insert(0, 'Maleek')
+# print(users)
+
+# users[2:2] = ['Joel', 'Umar', 'Lael']
+# print(users)
+
+# users[1:3] = ['Keith', 'Slam']
+# print(users)
+
+# users.remove('Rebirth')
+# print(users)
+
+# del users[0]
+# print(users)
+
+# users.sort()
+# print(users)
+
+# users.reverse()
+# print(users)
+
+# userscopy = users.copy()
+# my_users = list(users)
+# my_copy = users[:]
+
+# print(type(users))
+
+# Tuples
+
+mytuple = tuple(('Dave', 42, True))
+
+anothertuple = (1, 4, 2, 8)
+
+print(mytuple)
+print(type(mytuple))
+print(type(anothertuple))
